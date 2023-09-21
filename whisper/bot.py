b456491b4e63d7f22dece1093265bd237abbdaec
Sammybams <samuelbamgbola@gmail.com>
2023-09-21 16:38:24 +0100

    Started implementation of logic for bot.

--- whisper/bot.py
+++ whisper/bot.py
@@ -8,8 +8,32 @@ from botbuilder.schema import ChannelAccount
 class MyBot(ActivityHandler):
     # See https://aka.ms/about-bot-activity-message to learn more about the message and other activity types.
 
+    
     async def on_message_activity(self, turn_context: TurnContext):
-        await turn_context.send_activity(f"You said '{ turn_context.activity.text }'")
+        # Check if bot was tagged to a message and respond accordingly
+        # Get message current user text was tagged to
+        message = turn_context.activity.text
+        bot_name = "whisper_to_text"
+        # Check if bot was tagged to message
+        if f"@{bot_name}" in message:
+            # If bot was tagged, respond with a message
+            await turn_context.send_activity(f"You tagged me in a message! You said '{ turn_context.activity.text }'")
+
+        elif "/transcribe" in message:
+            await turn_context.send_activity(f"You tagged me in a message! You said '{ turn_context.activity.text }'")
+
+        elif "/translate" in message:
+            await turn_context.send_activity(f"You tagged me in a message! You said '{ turn_context.activity.text }'")
+
+        elif "/summarize" in message:
+            await turn_context.send_activity(f"You tagged me in a message! You said '{ turn_context.activity.text }'")
+
+        else:
+            # If bot was not tagged, respond with a message
+
+        # await turn_context.send_activity(f"You said '{ turn_context.activity.text }'")
+
+        
 
     async def on_members_added_activity(
         self,
