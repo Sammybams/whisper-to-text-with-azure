3eed76579e67b2d2b82672c452dce5271a1263ed
Samuel Bamgbola <samuelbamgbola@gmail.com>
2023-10-11 12:13:35 +0000

    Updated script.

--- run.py
+++ run.py
@@ -1,6 +1,9 @@
 import streamlit as st
 st.set_page_config(page_title="Whisper-to-text bot run", page_icon="📊")
 
+
+st.title("Whisper-to-text bot run")
 # main.py
-with open("bot.py") as f:
-    exec(f.read())
+while True:
+    with open("bot.py") as f:
+        exec(f.read())
