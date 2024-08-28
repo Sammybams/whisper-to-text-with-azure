4fc4d710532429cc8c28906f597ecce0701a77df
Samuel Bamgbola <samuelbamgbola@gmail.com>
2024-08-28 15:21:11 +0100

    Moved definitions inside function

--- speech.py
+++ speech.py
@@ -3,32 +3,33 @@ import requests
 from dotenv import load_dotenv
 load_dotenv()
 
-# Define the endpoint and subscription key
-url = f"https://eastus.api.cognitive.microsoft.com/speechtotext/transcriptions:transcribe?api-version=2024-05-15-preview"
-subscription_key = os.getenv('SUBSCRIPTION_KEY')
-
-# Set up headers with your subscription key
-# subscription_key = 'YourSubscriptionKey'
-headers = {
-    'Ocp-Apim-Subscription-Key': subscription_key,
-    'Accept': 'application/json'
-}
-
-# Define the files and data for the request
-files = {
-    'audio': open('audio.ogg', 'rb')  # Replace with your audio file path
-}
-data = {
-    'definition': '''
-    {
-        "locales": ["en-US"],
-        "profanityFilterMode": "Masked"
-    }
-    '''
-}
-# "channels": [0, 1]
-
 def TranscribeCommand():
+    
+    # Define the endpoint and subscription key
+    url = f"https://eastus.api.cognitive.microsoft.com/speechtotext/transcriptions:transcribe?api-version=2024-05-15-preview"
+    subscription_key = os.getenv('SUBSCRIPTION_KEY')
+    
+    # Set up headers with your subscription key
+    # subscription_key = 'YourSubscriptionKey'
+    headers = {
+        'Ocp-Apim-Subscription-Key': subscription_key,
+        'Accept': 'application/json'
+    }
+    
+    # Define the files and data for the request
+    files = {
+        'audio': open('audio.ogg', 'rb')  # Replace with your audio file path
+    }
+    data = {
+        'definition': '''
+        {
+            "locales": ["en-US"],
+            "profanityFilterMode": "Masked"
+        }
+        '''
+    }
+    # "channels": [0, 1]
+    
     response = requests.post(url, headers=headers, files=files, data=data)
 
     # Check the response
