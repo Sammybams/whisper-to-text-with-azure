6aaf5fa594e48c723ccd97e91674c87ca63abcf9
Samuel Bamgbola <samuelbamgbola@gmail.com>
2024-08-28 15:13:52 +0100

    Changed infrastructure to Fast Transcription API in Azure AI Speech

--- speech.py
+++ speech.py
@@ -1,83 +1,46 @@
-from dotenv import load_dotenv
-from datetime import datetime
-from moviepy.editor import *
 import os
-import time
-# Import namespaces
-import azure.cognitiveservices.speech as speech_sdk
-
-# Get Configuration Settings
+import requests
+from dotenv import load_dotenv
 load_dotenv()
-cog_key = os.getenv('COG_SERVICE_KEY')
-cog_region = os.getenv('COG_SERVICE_REGION')
 
-# Configure speech service
-speech_config = speech_sdk.SpeechConfig(cog_key, cog_region)
-# print('Ready to use speech service in:', speech_config.region)
+# Define the endpoint and subscription key
+url = f"https://eastus.api.cognitive.microsoft.com/speechtotext/transcriptions:transcribe?api-version=2024-05-15-preview"
+subscription_key = os.getenv('SUBSCRIPTION_KEY')
+
+# Set up headers with your subscription key
+# subscription_key = 'YourSubscriptionKey'
+headers = {
+    'Ocp-Apim-Subscription-Key': subscription_key,
+    'Accept': 'application/json'
+}
+
+# Define the files and data for the request
+files = {
+    'audio': open('audio.ogg', 'rb')  # Replace with your audio file path
+}
+data = {
+    'definition': '''
+    {
+        "locales": ["en-US"],
+        "profanityFilterMode": "Masked"
+    }
+    '''
+}
+# "channels": [0, 1]
 
 def TranscribeCommand():
-    command = ''
-    output_file = "audio.wav"
-    audioclip = AudioFileClip("audio.ogg")
-    audio_params = {
-        "codec": "pcm_s16le",
-        "fps": 16000,  # Set the desired sampling rate: 16000 Hz
-        # "fps": 8000,  # Alternatively, set the sampling rate to 8000 Hz
-        "nchannels": 1,  # Mono audio
-        "bitrate": "16k"  # Set the desired bitrate
-    }
+    response = requests.post(url, headers=headers, files=files, data=data)
 
-
-    audioclip.write_audiofile(output_file, codec=audio_params["codec"],fps=audio_params["fps"],nbytes=2,bitrate=audio_params["bitrate"])
-    
-    # Configure speech recognition
-    audio_config = speech_sdk.AudioConfig(filename="audio.wav")
-    # audio_config = speech_sdk.AudioConfig(use_default_microphone=True)
-    # print('Unable to recognize speech.')
-    
-    speech_recognizer = speech_sdk.SpeechRecognizer(speech_config, audio_config)
-    # print('Unable to recognize speech.')
-
-    # use Start Continuous Recognition to process long audio file
-    # speech_recognizer.start_continuous_recognition()
-    # print('Unable; to recognize speech.')
-    # time.sleep(10)
-    # speech_recognizer.stop_continuous_recognition()
-    done = False
-    def stop_cb(evt: speech_sdk.SpeechRecognitionEventArgs):
-        """callback that signals to stop continuous recognition upon receiving an event `evt`"""
-        nonlocal done
-        done = True
-
-    all_texts = []
-    def handle_final_result(evt):
-        all_texts.append(evt.result.text)
-
-    # Connect callbacks to the events fired by the speech recognizer
-    speech_recognizer.recognized.connect(handle_final_result)
-    # speech_recognizer.recognizing.connect(lambda evt: print('RECOGNIZING: {}'.format(evt)))
-    # speech_recognizer.recognized.connect(lambda evt: print('RECOGNIZED: {}'.format(evt)))
-    # speech_recognizer.session_started.connect(lambda evt: print('SESSION STARTED: {}'.format(evt)))
-    # speech_recognizer.session_stopped.connect(lambda evt: print('SESSION STOPPED {}'.format(evt)))
-    # speech_recognizer.canceled.connect(lambda evt: print('CANCELED {}'.format(evt)))
-
-    # stop continuous recognition on either session stopped or canceled events
-    speech_recognizer.session_stopped.connect(stop_cb)
-    speech_recognizer.canceled.connect(stop_cb)
-
-    # Start continuous speech recognition
-    speech_recognizer.start_continuous_recognition()
-    while not done:
-        time.sleep(.5)
-
-    speech_recognizer.stop_continuous_recognition()
-    # print(speech_sdk.SessionEventArgs.result.text)
-    # </SpeechContinuousRecognitionWithFile>
-    
-    # print("\n".join(all_texts))
-    # Process speech input
-    return "\n".join(all_texts)
-
-
-if __name__ == "__main__":
-    main()
+    # Check the response
+    if response.status_code == 200:
+        print('Success!')
+        # print(response.json())
+        # print(response.json()['combinedPhrases'][0]['text'])
+        return response.json()['combinedPhrases'][0]['text']
+        
+    else:
+        error_message = ""
+        # print('Error:', response.status_code)
+        # print(response.text)
+        error_message += f'Error: {response.status_code}\n{response.text}'
+        return error_message
